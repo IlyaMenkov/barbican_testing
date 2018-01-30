a5f3c2dc8d17bd9b77ecebfda9ce00726f37087b
Ilya Menkov <imenkov@mirantis.com>
2018-01-30 16:03:23 +0400

    woops fix

--- test_create_instance_not_signed_image.sh
+++ test_create_instance_not_signed_image.sh
@@ -11,14 +11,14 @@ ln -s /usr/share/salt-formulas/reclass/service/dogtag /srv/salt/reclass/classes/
 # Create /srv/salt/reclass/classes/cluster/virtual-mcp11-aio/openstack/dogtag.yml
 cp dogtag.yaml /srv/salt/reclass/classes/cluster/virtual-mcp11-aio/openstack/
 
-init_file="/srv/salt/reclass/classes/cluster/virtual-mcp11-aio/openstack/init.yml"
+init_file='/srv/salt/reclass/classes/cluster/virtual-mcp11-aio/openstack/init.yml'
 
 # Apply some changes
-sed -i 's/service.barbican.server.plugin.simple_crypto/- service.barbican.server.plugin.dogtag/g' init_file
-sed -i '/- service.barbican.server.plugin.dogtag/a - cluster.virtual-mcp11-aio.openstack.dogtag' init_file
-sed -i '/crypto_plugin: simple_crypto/d' init_file
-sed -i 's/store_plugin: store_crypto/store_plugin: dogtag_crypto/g' init_file
-sed -i 's/barbican_integration_enabled: False/barbican_integration_enabled: True/g' init_file
+sed -i 's/-service.barbican.server.plugin.simple_crypto/- service.barbican.server.plugin.dogtag/g' $init_file
+sed -i '/- service.barbican.server.plugin.dogtag/a - cluster.virtual-mcp11-aio.openstack.dogtag' $init_file
+sed -i '/crypto_plugin: simple_crypto/d' $init_file
+sed -i 's/store_plugin: store_crypto/store_plugin: dogtag_crypto/g' $init_file
+sed -i 's/barbican_integration_enabled: False/barbican_integration_enabled: True/g' $init_file
 
 # Check if Reclass render is fine
 reclass-salt --top
@@ -55,11 +55,11 @@ then
 fi
 
 wget http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
-glance image-create --name cirros --visibility public --disk-format qcow2 \
+glance image-create --name test_barbican --visibility public --disk-format qcow2 \
   --container-format bare --file cirros-0.3.5-x86_64-disk.img --progress
 
 # Boot instance
-nova boot --flavor m1.extra_tiny --image cirros --nic net-id=d23f9845-cbce-47a6-be15-0603f6a31365 test_barbican
+nova boot --flavor m1.extra_tiny --image test_barbican --nic net-id=d23f9845-cbce-47a6-be15-0603f6a31365 test_barbican
 
 # Check that instance in ERROR state
 check_error_state=`nova list | awk '/test/ && /ERROR/'`
